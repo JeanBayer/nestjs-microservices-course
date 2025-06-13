60ba66f096b04dab2496867740024998f8200826
Jean Bayer <jhanbayer@gmail.com>
2025-06-12 21:08:11 -0400

    feat: implementar microservicio de órdenes con NestJS, agregar DTOs y controlador

--- 02-products-app/orders-ms/src/main.ts
+++ 02-products-app/orders-ms/src/main.ts
@@ -1,8 +1,30 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { envs } from './config/envs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = new Logger('main');
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.PORT,
+      },
+    },
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  logger.log(`Orders microservice is running on port ${envs.PORT}`);
+  await app.listen();
 }
 bootstrap();
